d764acb7b5f1db4ae93f376f0fc882c45cf730af
Luca Sartori <lucasartori94@gmail.com>
2020-08-10 21:39:07 -0300

    Assignment 7

--- assignment-mta/index.js
+++ assignment-mta/index.js
@@ -19,11 +19,12 @@ const logger = winston.createLogger({
   format: winston.format.simple(),
   transports: [
     //
-    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
     new winston.transports.File({ filename: logDir + '/error.log', level: 'error' }),
-    new winston.transports.File({ filename: logDir + '/combined.log' })
+    new winston.transports.File({ filename: logDir + '/combined.log' }),
+    new winston.transports.Console({ format: winston.format.simple()})
   ]
 });
 
